cc79feec90614c44a2f641564049e5e1f3b4c440
wiibros220 <wiibros220@users.noreply.github.com>
2020-09-17 14:21:46 -0700

    Started Development of Le Monarque

---
+++ Items/LeMonarque.cs
@@ -0,0 +1,42 @@
+﻿using BeeBrosMod.Projectiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeeBrosMod.Items
+{
+    public class LeMonarque : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Le Monarque");
+            Tooltip.SetDefault("\"Wings flutter. \n Beauty distracts.Poison injects. \n The butterfly's curse extends to your enemies. \n A short life, shortened further by your hand.\" \n —Ada-1");
+        }
+
+        public override void SetDefaults()
+        {
+            item.ranged = true;
+            item.damage = 25;
+            item.shoot = mod.ProjectileType("LeArrow");
+            item.shootSpeed = 15;
+            item.width = 40;
+            item.height = 40;
+            item.useTime = 25;
+            item.useAnimation = 20;
+            item.useStyle = 5;
+            item.knockBack = 6;
+            item.value = 10;
+            item.rare = 5;
+            item.UseSound = SoundID.Item39;
+            item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/LeArrow.cs
@@ -0,0 +1,42 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeeBrosMod.Projectiles
+{
+    public class LeArrow : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Le Arrow");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.arrow = true;
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.aiStyle = 1;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            aiType = ProjectileID.JestersArrow;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            base.OnHitNPC(target, damage, knockback, crit);
+            target.AddBuff(BuffID.Poisoned, 400);
+            if (target.poisoned)
+            {
+                float projectilespeedX = 0;
+                float projectilespeedY = 0;
+                float projectileKnockBack = 0;
+                int projectiledamage = 15;
+                {
+                    Projectile.NewProjectile(target.position.X, target.position.Y, projectilespeedX, projectilespeedY, mod.ProjectileType("ToxicFlask"), projectiledamage, projectileKnockBack, projectile.owner, 0, 0);
+                }
+            }
+        }
+
+
+    }
+}
