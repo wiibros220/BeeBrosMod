cdedfcd8b23f4c75bfd4ff04da28d90031312211
murdersheep <joshwgarza@gmail.com>
2020-09-18 13:32:01 -0700

    just uhhh, yeah

--- Projectiles/Nousagi.cs
+++ Projectiles/Nousagi.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria.ModLoader;
 using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria;
 
 namespace BeeBrosMod.Projectiles
 {
@@ -17,18 +19,17 @@ namespace BeeBrosMod.Projectiles
 
         public override void SetDefaults()
         {
-            //float projectileSpeed = 10;
             
             projectile.width = 25;
             projectile.height = 25;
             projectile.friendly = true;
             projectile.ranged = true;
-            projectile.velocity = new Microsoft.Xna.Framework.Vector2(1000000);
+          
         }
 
         public override void AI()
         {
-            projectile.ai[0] += 1f; 
+            projectile.ai[0] += 1f;
             if (projectile.ai[0] >= 15f)
             {
                 projectile.ai[0] = 15f;
@@ -40,6 +41,11 @@ namespace BeeBrosMod.Projectiles
             }
 
             projectile.rotation += 0.4f * (float)projectile.direction;
+        }
+
+        public override void OnHitNPC(Terraria.NPC target, int damage, float knockback, bool crit)
+        {
+            Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0, 0, ProjectileID.Bunny, 40, 10, Terraria.Main.myPlayer, 0f, 0f);
         }
     }
 }
